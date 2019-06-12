216ae1deedb7e84333bf96f8b6e59ed40473c0c2
jawad727 <jawaddhussein@gmail.com>
2019-06-12 16:34:15 -0700

    Finished second challenge

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+
+  minimum_number = 1000000
+
+  for key in recipe:
+    if key in ingredients:
+
+      eachBatch = ingredients[key] / recipe[key]
+      if eachBatch < minimum_number:
+        minimum_number = int(eachBatch)
+
+    else:
+      minimum_number = 0
+
+  return minimum_number
 
 # The function takes in a recipe dictionary and a ingredients dictionary
 # It needs iterate through the keys in the first dictionary and match them with the keys of the second
