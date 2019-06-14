550ed3069b63f5870c61d140475f680f339a2a4d
jawad727 <jawaddhussein@gmail.com>
2019-06-13 21:26:53 -0700

    RPS Works but test fails due to recursion limit

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  options = ['rock', 'paper', 'scissors']
+  allPossibilities = []
 
+  def roundChoice(round, roundNumber):#creating a function within a function to call later
+    for i in options:#iterating through options (rps)
+      round.append(i);#pushing each option into the empty array (round)
+      if roundNumber == n:# if roundNumber equals n 
+        allPossibilities.append(round[:])# 
+      else:
+        roundChoice(round, roundNumber + 1)
+      round.pop()
+
+  roundChoice([], 1)
+  return allPossibilities
+
+print(rock_paper_scissors(1))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
